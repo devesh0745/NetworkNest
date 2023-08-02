37773ca5d24933203a043dc99b1b2463d003631d
devesh0745 <sdevesh227@gmail.com>
2023-08-02 19:59:51 +0530

    layout and partial added

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts');
+
+//Use before routes.
+app.use(expressLayouts);
+
+
 const db=require('./config/mongoose');
 //use express router(('/' means any request)Any request that comes goes to routes)
+
 app.use('/',require('./routes/index'));
 
 //Setting up view engine
@@ -13,4 +20,4 @@ app.set('views','./views')
 app.listen(port,function(err){
     if(err){console.log(`Error in running server:${err}`)};
     console.log(`Server running on port:${port}`);
-});
+})
