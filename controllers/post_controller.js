d24e796a42f9d93984aa7b11f6a1e5d7880b1b08
devesh0745 <sdevesh227@gmail.com>
2023-08-09 00:55:06 +0530

    post and comment associated with post deleted

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comments');
 
 module.exports.create=async function(req,res){
     try{
@@ -14,5 +15,39 @@ module.exports.create=async function(req,res){
     catch(err){
         console.log("Error in posting!");
         return;
+    }
+}
+
+//to delete post and all the comments on the post
+module.exports.destroy=async function(req,res){
+    const post=await Post.findById(req.params.id);
+    //console.log(typeof(post.user.toString()),post.user.toString());
+    //console.log(typeof(req.user.id),req.user.id);
+    try{
+        //.id will convert the id of the user into string
+        //For authentication.
+    
+        if(post.user.toString()==req.user.id){
+            post.deleteOne();
+            console.log('post deleted')
+
+
+            try{
+            //All the comments which are associted with this post will be deleted.
+            Comment.deleteMany({post:req.params.id});
+            console.log('comment deleted');
+            return res.redirect('back');
+            }
+            catch(err){
+                console.log('Error in deleting comments');
+            }
+        }else{
+            alert('please login first');
+            return res.redirect('back');
+            
+        }
+    }
+    catch(err){
+        console.log('error in deleting post');
     }
 }
