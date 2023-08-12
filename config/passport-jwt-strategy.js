d6377658db1ce1992e4ec82d06a4c50125f84c81
devesh0745 <sdevesh227@gmail.com>
2023-08-13 01:27:09 +0530

    setting up passport jwt

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,28 @@
+const passport=require('passport');
+const JWTStrategy=require('passport-jwt').Strategy;
+//It will extract jwt token from the header
+const ExtractJWT=require('passport-jwt').ExtractJwt
+
+const User=require('../models/user');
+
+let opts={
+    jwtFromRequest:ExtractJWT.fromAuthHeaderAsBearerToken,
+    secretOrKey : 'AllSocial'
+}
+
+//Now it will tell passport to use jwt.//this call back function reads data from the jwt pay load.
+passport.use(new JWTStrategy(opts,async function(jwtPayLoad,done){
+    try{
+    const user=await User.findById(jwtPayLoad._id);
+    if(user){
+        return done(null,user);
+        }else{
+            return done(null,false);
+        }
+    }
+    catch(err){
+        console.log("Error in finding user from JWT");
+    }
+}));
+
+module.exports = passport;
